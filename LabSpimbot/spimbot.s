f18b177c02e9926c43b7f18e5b0d1dee1949733c
Bennett Ip <ngai0408@gmail.com>
2016-12-04 15:21:54 -0600

    Add non-main code from Lab10

--- LabSpimbot/spimbot.s
+++ LabSpimbot/spimbot.s
@@ -48,10 +48,67 @@ REQUEST_PUZZLE_ACK      = 0xffff00d8
 REQUEST_PUZZLE_INT_MASK = 0x800
 
 .data
-# data things go here
+// BEN
+flagFire:       .word 0
+flagWhatever:   .word 0 // TEMPLETE: flags for interrupts
+// BEN
 
 .text
 main:
 	# go wild
 	# the world is your oyster :)
 	j	main
+
+// BEN
+.kdata                              # interrupt handler data (separated just for readability)
+chunkIH:	.space 8                # space for two registers
+
+
+.ktext 0x80000180
+interrupt_handler:
+.set noat
+    move	$k1, $at                # Save $at # don't change k1!
+.set at
+    la      $k0, chunkIH
+    sw      $a0, 0($k0)             # Get some free registers
+    sw      $a1, 4($k0)             # by storing them to a global variable
+
+interrupt_dispatch:                 # Interrupt:
+    mfc0	$k0, $13                # Get Cause register, again
+    beq     $k0, 0, done            # handled all outstanding interrupts
+
+    and     $a0, $k0, ON_FIRE_MASK  # is there a on-fire interrupt?
+    bne     $a0, 0, on_fire_interrupt
+
+    and     $a0, $k0, SOME_MASK
+    bne     $a0, 0, some_interrupt  # TEMPLATE: some other interrupts
+
+on_fire_interrupt:
+    sw      $0, ON_FIRE_ACK         # acknowledge interrupt
+    # get location
+    lw      $k0, GET_FIRE_LOC
+
+    srl     $a0, $k0, 16
+    sw      $a0, X
+    sll     $a0, $k0, 16
+    srl     $a0, $a0, 16
+    sw      $a0, Y
+    li      $k0, 1
+    sw      $k0, flag               # flag = 1
+
+    j	interrupt_dispatch          # see if other interrupts are waiting
+
+some_interrupt:                     # TEMPLETE
+    sw  $0, SOME_ACK
+# code
+    j   interrupt_dispatch
+
+done:
+    la      $k0, chunkIH
+    lw      $a0, 0($k0)
+    lw      $a1, 4($k0)             # Restore saved registers
+.set noat
+    move	$at, $k1                # Restore $at
+.set at
+    eret
+// BEN
